05fda1b1f00ce1312263f8246cac59ea385a7204
Kozlovtseva <alina.lm@mail.ru>
2019-12-30 15:31:14 +0300

    additional styles for tap icon

--- components/intro/Argument.js
+++ components/intro/Argument.js
@@ -3,7 +3,18 @@ import css from "./Argument.scss";
 const Argument = ({ item }) => (
   <div className={css.Argument}>
     <div className={css.Icon}>
-      <img src={`/icons/${item.icon}.svg`} alt={item.icon} />
+      {item.icon === "tap" ? ( //для отдельной стилизации пункта (есть наклон и дополнительный символ)
+        <div className={css.TapIcon}>
+          <img
+            className={css.TapImage}
+            src={`/icons/${item.icon}.svg`}
+            alt={item.icon}
+          />
+          <div className={css.Cross} />
+        </div>
+      ) : (
+        <img src={`/icons/${item.icon}.svg`} alt={item.icon} />
+      )}
     </div>
     <div className={css.Text}>{item.text}</div>
   </div>
