8ea0f1d59ad017383bd29dff29e200583e6dbf71
Kozlovtseva <alina.lm@mail.ru>
2019-12-30 11:51:07 +0300

    add PlayButton

--- components/UI/PlayButton.js
+++ components/UI/PlayButton.js
@@ -1,7 +1,17 @@
+import css from "./PlayButton.scss";
+
 const PlayButton = ({ onClick }) => (
-  <>
-    <div onClick={onClick} />
-  </>
+  <div className={css.External}>
+    <div className={css.Internal}>
+      <div className={css.Back}>
+        <img
+          // className={css.LampImage}
+          src="/icons/media-play-symbol.svg"
+          alt="lamp"
+        />
+      </div>
+    </div>
+  </div>
 );
 
 export default PlayButton;
